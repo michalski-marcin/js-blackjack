437999cd8e4d05bd0850201762c1116f566c1f5b
Marcin Michalski <mkamichalski@gmail.com>
2023-09-11 12:53:43 +0200

    Project finished

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-const cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
+
+
 
 
 // GET ELEMENTS FROM HTML FILE
@@ -13,7 +13,6 @@ const messageEl = document.getElementById("message-el")
 const sumEl = document.getElementById("sum-el")
 const cardsEl = document.getElementById("cards-el")
 
-
 // MAIN GAME FUNCTION
 
 function renderGame() {
@@ -21,7 +20,7 @@ function renderGame() {
     for (let i = 0; i < cards.length; i++) {
         cardsEl.textContent += `${cards[i]} `
     }
-    
+
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -38,20 +37,36 @@ function renderGame() {
 
 // FUNCTIONS
 
-function getRandomCard () {
-    let randomCard = Math.floor(Math.random() * 13) + 1
-    return randomCard
-}
-
 function startGame() {
+    hasBlackJack = false
+    cards = []
+    sum = 0
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
 
+function getRandomCard() {
+    let randomCard = Math.floor(Math.random() * 13) + 1
+    if (randomCard == 1) {
+        return 11
+    } else if (randomCard > 10) {
+        return 10
+    } else {
+        return randomCard
+    }
+}
+
 function newCard() {
-    let newCard = getRandomCard();
-    sum += newCard;
-    cards.push(newCard);
-    renderGame();
+    if (isAlive && !hasBlackJack) {
+        let newCard = getRandomCard();
+        sum += newCard;
+        cards.push(newCard);
+        renderGame();
+    }
 }
 
 
